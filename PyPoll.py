f27586afd8d2dadb73f51b6447b286fc641e5792
Stephanie Juniper <Stephanie@Stephanies-MacBook-Air.local>
2020-11-05 17:25:12 -0500

    adding PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -27,27 +27,27 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
 #print the header row
-headers = next(file_reader)
-print(headers)
-# #print each row in the csv file.
-#     for row in file_reader:
+    headers = next(file_reader)
+    print(headers)
+#print each row in the csv file.
+    for row in file_reader:
 
-#         print(row)
+        print(row)
 
-# #print the file object.
-#     print(election_data)
+#print the file object.
+    print(election_data)
 
 
 
-#using the open() function with the w mode we will write data to the file
-#open(file_to_save, "w")
-#using the with statement ope the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    #write some data to that file
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("---------------------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+# #using the open() function with the w mode we will write data to the file
+# #open(file_to_save, "w")
+# #using the with statement ope the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+#     #write some data to that file
+#     txt_file.write("Counties in the Election\n")
+#     txt_file.write("---------------------------\n")
+#     txt_file.write("Arapahoe\nDenver\nJefferson")
 
-# #close the file
-# outfile.close()
+# # #close the file
+# # outfile.close()
 
